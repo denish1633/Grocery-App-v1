dde8736f4fa11a7de816640f6440910e70c428ea
denish <denishshingala1632003@gmail.com>
2025-12-31 12:08:52 -0500

    Hosting ready

--- backend/app/main.py
+++ backend/app/main.py
@@ -6,6 +6,7 @@ from app.api.v1 import products
 from app.api.v1 import cart
 from app.api.v1 import orders
 from app.api.v1 import external_products
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Ecommerce API")
 
@@ -21,6 +22,17 @@ app.include_router(cart.router, prefix="/api/v1")
 app.include_router(orders.router, prefix="/api/v1")
 app.include_router(external_products.router, prefix="/api/v1")
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "https://denish1633.github.io",
+        "http://localhost:3000",   # optional for local web
+        "http://localhost:8000",   # optional for testing
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def root():
